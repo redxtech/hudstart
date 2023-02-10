6eb7ccd52a1247d51244e9a581c66a9941581b50
Gabe Dunn <gabe@sent.at>
2023-02-10 14:51:37 -0800

    add initial frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <main id="overlay">
+		<overlay
+			:p1-name="one.name"
+			:p1-tag="one.tag"
+			:p1-score="one.score"
+			:p1-char="one.char"
+			:p1-winners="two.winners"
+			:p2-name="two.name"
+			:p2-tag="two.tag"
+			:p2-score="two.score"
+			:p2-char="two.char"
+			:p2-winners="two.winners"
+			:match="match"
+			:best-of="bestOf"
+			:grands="grands"
+		/>
+	</main>
+</template>
+
+<script>
+import overlay from './components/overlays/default.vue'
+
+export default {
+	data() {
+		return {
+			one: {
+				name: 'player one',
+				tag: 'ssb',
+				score: 2,
+				char: 'captain falcon',
+				winners: true
+			},
+			two: {
+				name: 'player two',
+				tag: 'ssb',
+				score: 1,
+				char: 'samus',
+				winners: false
+			},
+			match: 'grand finals',
+			grands: true,
+			bestOf: 5
+		}
+	},
+	components: {
+		overlay
+	}
+}
+</script>
+
+<style scoped>
+#overlay {
+	height: 1080px;
+	width: 1920px;
+	border: 5px red solid;
+}
+</style>
---
+++ frontend/src/components/overlays/default.vue
@@ -0,0 +1,56 @@
+<template>
+<div id="default-overlay">
+	<div>
+		<h1>{{ match }}, best of {{ bestOf }}</h1>
+	</div>
+	<div>
+		<h2>entrant one</h2>
+		<p>name: {{ p1Name }}</p>
+		<p>tag: {{ p1Tag }}</p>
+		<p>char: {{ p1Char }}</p>
+		<p>score: {{ p1Score }}</p>
+		<p v-if="grands">winners: {{ p1Winners }}</p>
+	</div>
+	<div>
+		<h2>entrant two</h2>
+		<p>name: {{ p2Name }}</p>
+		<p>tag: {{ p2Tag }}</p>
+		<p>char: {{ p2Char }}</p>
+		<p>score: {{ p2Score }}</p>
+		<p v-if="grands">winners: {{ p2Winners }}</p>
+	</div>
+</div>
+</template>
+
+<script>
+export default {
+	props: {
+		p1Name: { type: String, required: true },
+		p1Tag: { type: String, required: true },
+		p1Score: { type: Number, required: true },
+		p1Char: { type: String, required: true },
+		p1Winners: { type: Boolean, required: true },
+		p2Name: { type: String, required: true },
+		p2Tag: { type: String, required: true },
+		p2Score: { type: Number, required: true },
+		p2Char: { type: String, required: true },
+		p2Winners: { type: Boolean, required: true },
+		match: { type: String, required: true },
+		bestOf: { type: Number, default: 3 },
+		grands: { type: Boolean, default: false },
+	}
+}
+</script>
+
+<style scoped>
+#default-overlay {
+  color: #213547;
+  text-align: center;
+}
+
+h1 {
+  font-size: 3.2em;
+  line-height: 1.1;
+}
+</style>
+
